7afedc538ad7a9f88cb4c3530f357aafa7bcb0e4
Javier Aso Burgos <asoycia@hotmail.com>
2020-11-06 23:09:52 +0100

    muestro los items por pantalla

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Equipment;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class EquipmentController extends Controller
+
+
 {
     public function index()
     {
@@ -20,8 +22,13 @@ class EquipmentController extends Controller
     public function store()
     {
 
-        echo session('COD_PJ');
-        echo session('CLASS_PJ');
+        $items = DB::select('SELECT * FROM EQUIPMENT WHERE COD_EQUIPMENT NOT IN (SELECT COD_EQUIPMENT FROM WEAPON) AND COD_EQUIPMENT NOT IN (SELECT COD_EQUIPMENT FROM ARMOR);');
+        $armors = DB::select('SELECT EQUIPMENT.COD_EQUIPMENT,EQUIPMENT.NAME, EQUIPMENT.PRICE, ARMOR.DEFENSE FROM (EQUIPMENT INNER JOIN ARMOR ON EQUIPMENT.COD_EQUIPMENT = ARMOR.COD_EQUIPMENT);');
+        $weaponsM = DB::select('SELECT EQUIPMENT.COD_EQUIPMENT,EQUIPMENT.NAME, EQUIPMENT.PRICE, WEAPON.DAMAGE FROM (EQUIPMENT INNER JOIN WEAPON ON EQUIPMENT.COD_EQUIPMENT = WEAPON.COD_EQUIPMENT);');
+        $weaponsR = DB::select('SELECT EQUIPMENT.COD_EQUIPMENT,EQUIPMENT.NAME, EQUIPMENT.PRICE, WEAPON.DAMAGE, RANGED.RANG, RANGED.SHOTS FROM ((EQUIPMENT INNER JOIN WEAPON ON EQUIPMENT.COD_EQUIPMENT = WEAPON.COD_EQUIPMENT) INNER JOIN RANGED ON RANGED.COD_EQUIPMENT = EQUIPMENT.COD_EQUIPMENT);');
+
+
+        return view('equipment', ['items' => $items , 'armors' => $armors , 'weaponsM' => $weaponsM , 'weaponsR' => $weaponsR]);
 
 
     }
--- resources/views/equipment.blade.php
+++ resources/views/equipment.blade.php
@@ -1,6 +1,82 @@
-<h3>Datos de equipo </h3>
-@foreach($equipment as $item)
-   <li>
-      {{ $item->NAME.' '.$item->PRICE }}
-    </li>
+@extends('layout')
+@section('titulo')
+Tienda
+@endsection
+@section('contenido')
+<table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th>Nombre item</th>
+        <th>Precio</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach($items as $item)
+        <tr>
+            <td>{{$item->NAME}}</td>
+            <td>{{$item->PRICE}}</td>
+          </tr>
 @endforeach
+    </tbody>
+  </table>
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th>Nombre item</th>
+        <th>Precio</th>
+        <th>Defensa</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach($armors as $item)
+        <tr>
+            <td>{{$item->NAME}}</td>
+            <td>{{$item->PRICE}}</td>
+            <td>{{$item->DEFENSE}}</td>
+          </tr>
+@endforeach
+    </tbody>
+  </table>
+</table>
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th>Nombre item</th>
+      <th>Precio</th>
+      <th>Daño</th>
+    </tr>
+  </thead>
+  <tbody>
+      @foreach($weaponsM as $item)
+      <tr>
+          <td>{{$item->NAME}}</td>
+          <td>{{$item->PRICE}}</td>
+          <td>{{$item->DAMAGE}}</td>
+        </tr>
+@endforeach
+  </tbody>
+</table>
+<table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th>Nombre item</th>
+        <th>Precio</th>
+        <th>Daño</th>
+        <th>Tiros</th>
+        <th>Alcance</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach($weaponsR as $item)
+        <tr>
+            <td>{{$item->NAME}}</td>
+            <td>{{$item->PRICE}}</td>
+            <td>{{$item->DAMAGE}}</td>
+            <td>{{$item->SHOTS}}</td>
+            <td>{{$item->RANG}}</td>
+          </tr>
+  @endforeach
+    </tbody>
+  </table>
+@endsection
+
